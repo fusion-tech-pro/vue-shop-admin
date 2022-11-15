bb16f3d2ca354344e45e48fe4d9e48d9156f7c05
Олеся Деревянко <olesya.derevyanko@fusion-tech.pro>
2022-11-15 10:27:05 +0300

    added logout function to the pop-up menu

--- src/pages/home/components/UserMenu.vue
+++ src/pages/home/components/UserMenu.vue
@@ -6,7 +6,7 @@
       </q-item-section>
     </q-item>
 
-    <q-item clickable v-close-popup>
+    <q-item clickable v-close-popup @click="onLogout">
       <q-item-section>
         <q-item-label>Logout</q-item-label>
       </q-item-section>
@@ -14,6 +14,19 @@
   </q-list>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useUserStore } from "@/stores/user";
+import { useRouter } from "vue-router";
+
+const userStore = useUserStore();
+const router = useRouter();
+
+async function onLogout() {
+  await userStore.logout();
+  router.replace({
+    name: "sign-in",
+  });
+}
+</script>
 
 <style scoped></style>
