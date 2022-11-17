3768085a5c4cfec724b5656a0a0e512fe45f34d3
ilya-Varfolomeev <ilya.varfolomeev@fusion-tech.pro>
2022-11-17 12:42:27 +0300

    Improve ProductsTable types,functionality

--- src/pages/products/components/ProductsTable.vue
+++ src/pages/products/components/ProductsTable.vue
@@ -1,30 +1,33 @@
 <template>
-  <div class="">
-    <q-table
-      :rows="rows"
-      :columns="columns"
-      row-key="name"
-      class="my-sticky-header-table"
-    />
+  <div v-if="props.productsRow !== null">
+    <q-table :rows="props.productsRow" :columns="columns" row-key="name" />
   </div>
 </template>
-<script lang="ts">
-const columns = [
+<script lang="ts" setup>
+import type { Product } from "@/core/models/Product";
+import type { QTableProps } from "quasar";
+import { defineProps } from "vue";
+
+const props = defineProps<{
+  productsRow: Product[] | null;
+}>();
+
+const columns: QTableProps["columns"] = [
   {
     name: "image",
     required: true,
     label: "Image",
     align: "left",
-    field: (row) => row.image,
-    format: (val) => `${val}`,
+    field: (row: Product) => row.image,
+    format: (val: string) => `${val}`,
   },
   {
     name: "name",
     required: true,
     label: "Name",
     align: "left",
-    field: (row) => row.name,
-    format: (val) => `${val}`,
+    field: (row: Product) => row.name,
+    format: (val: string) => `${val}`,
     sortable: true,
   },
   {
@@ -35,9 +38,21 @@ const columns = [
     sortable: true,
   },
   { name: "shop", label: "Shop", field: "shop" },
-  { name: "productType", label: "Product Type", field: "productType" },
-  { name: "price", label: "Price/Unit", field: "price", sortable: true },
-  { name: "quantity", label: "Quantity", field: "quantity", sortable: true },
+  { name: "productType", label: "Product Type", field: "product_type" },
+  {
+    name: "price",
+    label: "Price/Unit",
+    field: "price",
+    sortable: true,
+    sort: (a: string, b: string) => parseInt(a, 10) - parseInt(b, 10),
+  },
+  {
+    name: "quantity",
+    label: "Quantity",
+    field: "quantity",
+    sortable: true,
+    sort: (a: string, b: string) => parseInt(a, 10) - parseInt(b, 10),
+  },
   {
     name: "status",
     label: "Status",
@@ -49,118 +64,4 @@ const columns = [
     field: "actions",
   },
 ];
-
-const rows = [
-  {
-    name: "Frozen Yogurt",
-    calories: 159,
-    fat: 6.0,
-    carbs: 24,
-    protein: 4.0,
-    sodium: 87,
-    calcium: "14%",
-    iron: "1%",
-  },
-  {
-    name: "Ice cream sandwich",
-    calories: 237,
-    fat: 9.0,
-    carbs: 37,
-    protein: 4.3,
-    sodium: 129,
-    calcium: "8%",
-    iron: "1%",
-  },
-  {
-    name: "Eclair",
-    calories: 262,
-    fat: 16.0,
-    carbs: 23,
-    protein: 6.0,
-    sodium: 337,
-    calcium: "6%",
-    iron: "7%",
-  },
-  {
-    name: "Cupcake",
-    calories: 305,
-    fat: 3.7,
-    carbs: 67,
-    protein: 4.3,
-    sodium: 413,
-    calcium: "3%",
-    iron: "8%",
-  },
-  {
-    name: "Gingerbread",
-    calories: 356,
-    fat: 16.0,
-    carbs: 49,
-    protein: 3.9,
-    sodium: 327,
-    calcium: "7%",
-    iron: "16%",
-  },
-  {
-    name: "Jelly bean",
-    calories: 375,
-    fat: 0.0,
-    carbs: 94,
-    protein: 0.0,
-    sodium: 50,
-    calcium: "0%",
-    iron: "0%",
-  },
-  {
-    name: "Lollipop",
-    calories: 392,
-    fat: 0.2,
-    carbs: 98,
-    protein: 0,
-    sodium: 38,
-    calcium: "0%",
-    iron: "2%",
-  },
-  {
-    name: "Honeycomb",
-    calories: 408,
-    fat: 3.2,
-    carbs: 87,
-    protein: 6.5,
-    sodium: 562,
-    calcium: "0%",
-    iron: "45%",
-  },
-  {
-    name: "Donut",
-    calories: 452,
-    fat: 25.0,
-    carbs: 51,
-    protein: 4.9,
-    sodium: 326,
-    calcium: "2%",
-    iron: "22%",
-  },
-  {
-    name: "KitKat",
-    calories: 518,
-    fat: 26.0,
-    carbs: 65,
-    protein: 7,
-    sodium: 54,
-    calcium: "12%",
-    iron: "6%",
-  },
-];
-
-export default {
-  setup() {
-    return {
-      columns,
-      rows,
-    };
-  },
-};
 </script>
-
-<style></style>
