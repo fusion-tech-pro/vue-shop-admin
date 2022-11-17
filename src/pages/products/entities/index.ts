f5576887eb208fa0b2af5e3b9935282b14880a84
ilya-Varfolomeev <ilya.varfolomeev@fusion-tech.pro>
2022-11-17 14:37:22 +0300

    Implement AddNewProductFormState for AddProductModal creating new product

---
+++ src/pages/products/entities/index.ts
@@ -0,0 +1,10 @@
+export type AddNewProductFormState = {
+  image?: string;
+  name: string;
+  group?: string;
+  shop?: string;
+  productType?: string;
+  price?: number;
+  quantity?: number;
+  status?: string;
+};
