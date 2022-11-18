a59559753e1bf35d64dd186991ff6e9ea9cc318b
ilya-Varfolomeev <ilya.varfolomeev@fusion-tech.pro>
2022-11-18 22:52:59 +0300

    Improve layout,styles for products search bar

--- src/pages/products/ProductsPage.vue
+++ src/pages/products/ProductsPage.vue
@@ -1,50 +1,74 @@
 <template>
-  <div class="column q-pa-md q-mb-lg bg-white rounded-borders">
-    <p class="text-h6 text-center text-bold">Products</p>
-    <q-input
-      outlined
-      label="Type your query and press enter"
-      type="text"
-      class="q-mb-sm"
-      v-model.trim="searchProductText"
-    >
-      <template v-slot:prepend>
-        <q-icon name="search" color="grey" />
-      </template>
-    </q-input>
-    <q-btn
-      color="white"
-      text-color="primary"
-      label="Filter"
-      flat
-      @click="handleFilterClick"
-    />
+  <div class="products-page__search-bar-wrapper shadow-1">
+    <div class="products-page__search-bar">
+      <p class="products-page__search-bar-title">Products</p>
+      <q-input
+        outlined
+        label="Type your query and press enter"
+        type="text"
+        class="products-page__search-field"
+        dense
+        hide-bottom-space
+        v-model.trim="searchProductText"
+      >
+        <template v-slot:prepend>
+          <q-icon name="search" color="grey" />
+        </template>
+      </q-input>
+      <q-btn
+        color="white"
+        text-color="primary"
+        label="Filter"
+        class="products-page__button"
+        no-caps
+        dense
+        flat
+        @click="handleFilterClick"
+      >
+        <q-icon
+          :name="isFiltersOpen ? 'arrow_upward' : 'arrow_downward'"
+          size="xs"
+          class="q-ml-sm"
+        />
+      </q-btn>
+      <q-btn
+        color="white"
+        text-color="primary"
+        label="Add Product"
+        class="products-page__button"
+        no-caps
+        dense
+        flat
+        @click="isAddProductModal = true"
+      />
+    </div>
     <div
       v-if="isFiltersOpen"
       class="q-pt-md q-mt-md products-page__filters-wrapper"
     >
-      <q-input
-        outlined
-        label="Filter by Group"
-        type="text"
-        class="q-mb-sm"
-        v-model.trim="searchGroupText"
-      />
-      <q-input
-        outlined
-        label="Filter by Shop"
-        type="text"
-        class="q-mb-sm"
-        v-model.trim="searchShopText"
-      />
+      <div class="products-page__select-wrapper">
+        <label class="products-page__select-label">Filter By Group</label>
+        <q-select
+          outlined
+          v-model.trim="searchGroupText"
+          :options="options"
+          label="Filter by Group"
+          bg-color="white"
+          dense
+        />
+      </div>
+      <div class="products-page__select-wrapper">
+        <label class="products-page__select-label">Filter By Shop</label>
+        <q-select
+          outlined
+          v-model.trim="searchShopText"
+          :options="options"
+          label="Filter by Shop"
+          bg-color="white"
+          dense
+        />
+      </div>
     </div>
-    <q-btn
-      color="white"
-      text-color="primary"
-      label="Add Product"
-      flat
-      @click="isAddProductModal = true"
-    />
   </div>
 
   <ProductsTable :productsRow="filteredProducts" />
@@ -57,6 +81,7 @@
 </template>
 
 <script lang="ts">
+const options = [1, 2, 3, 5, 5];
 import type { Product } from "@/core/models/Product";
 import type { ProductFormState } from "./entities/index";
 import { useProductsStore } from "@/stores/products";
@@ -80,6 +105,7 @@ export default defineComponent({
       searchShopText: "",
       isFiltersOpen: false,
       isAddProductModal: false,
+      options: options,
     };
   },
   methods: {
@@ -128,8 +154,82 @@ export default defineComponent({
 });
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.products-page__search-bar-wrapper {
+  background: white;
+  padding: 20px;
+  margin-bottom: 32px;
+  border: 0 solid #ececec;
+  border-radius: 5px;
+
+  @include media-sm {
+    padding: 32px;
+  }
+}
+.products-page__search-bar {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  align-items: center;
+  justify-content: center;
+
+  @include media-sm {
+    display: grid;
+    grid-template-columns: 1fr 4fr repeat(2, max-content);
+    column-gap: 20px;
+  }
+}
+
+.products-page__search-bar-title {
+  font-size: 1.125rem;
+  line-height: 1.75rem;
+  font-weight: 600;
+  margin-bottom: 20px;
+  text-align: center;
+
+  @include media-sm {
+    text-align: left;
+    margin: 0;
+  }
+}
+
+.products-page__search-field {
+  width: 100%;
+  margin-bottom: 10px;
+
+  @include media-sm {
+    margin: 0;
+  }
+}
+.products-page__button {
+  width: fit-content;
+  justify-self: center;
+  padding: 0;
+  font-weight: 600;
+
+  @include media-sm {
+    justify-self: end;
+  }
+}
+
 .products-page__filters-wrapper {
   border-top: 1px solid #ececec;
+  display: grid;
+  row-gap: 20px;
+
+  @include media-sm {
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 20px;
+  }
+}
+.products-page__select-wrapper {
+  display: flex;
+  flex-direction: column;
+}
+.products-page__select-label {
+  margin-bottom: 12px;
+  font-weight: 600;
+  color: rgba(75, 85, 99, 1);
 }
 </style>
