2604382cb35431571143419005c6853d816904f3
ilya-Varfolomeev <ilya.varfolomeev@fusion-tech.pro>
2022-11-22 09:58:13 +0300

    Implement ShopsResponse

---
+++ src/data/dto/ShopsResponse.ts
@@ -0,0 +1,16 @@
+export interface ShopsResponse {
+  logo?: string;
+  cover_image?: string;
+  name: string;
+  owner_name?: string;
+  description?: string;
+  country?: string;
+  state?: string;
+  city?: string;
+  street?: string;
+  zip_code?: string;
+  telephone_number?: string;
+  total_products?: number;
+  total_orders?: number;
+  date_of_registration?: string;
+}
